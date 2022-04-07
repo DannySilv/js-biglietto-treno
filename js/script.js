5a8d750b5f28923d7bb7b53aa5e8960719060cef
DannySilv <danielesilvestre@outlook.it>
2022-04-07 16:32:54 +0200

    Added Cleaner Solution

--- js/script.js
+++ js/script.js
@@ -7,17 +7,22 @@ const ticketPrice = busRideKm * 0.21;
 console.log(ticketPrice);
 const under18Age = 18;
 const over65Age = 65;
-const ticketDiscount20 = (ticketPrice * 20) / 100;
-console.log(ticketDiscount20);
-const ticketDiscount40 = (ticketPrice * 40) / 100;
-console.log(ticketDiscount40);
+//PER IL SECONDO METODO
+// const ticketDiscount20 = (ticketPrice * 20) / 100;
+// console.log(ticketDiscount20);
+// const ticketDiscount40 = (ticketPrice * 40) / 100;
+// console.log(ticketDiscount40);
 
 let finalTicketPrice; 
 
 if (passengerAge < under18Age) {
-    finalTicketPrice = ticketPrice - ticketDiscount20;
+    finalTicketPrice = ticketPrice - ((ticketPrice * 20) / 100); //PRIMO METODO
+    // SECONDO METODO
+    // finalTicketPrice = ticketPrice - ticketDiscount20;
 } else if (passengerAge > over65Age) {
-    finalTicketPrice = ticketPrice - ticketDiscount40;
+    finalTicketPrice = ticketPrice - ((ticketPrice * 40) / 100); //PRIMO METODO
+    // SECONDO METODO
+    // finalTicketPrice = ticketPrice - ticketDiscount40;
 } else {
     finalTicketPrice = ticketPrice;
 }
