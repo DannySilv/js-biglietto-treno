3542e107bdb7159dec42d36e8cda8e0add04ef41
DannySilv <danielesilvestre@outlook.it>
2022-04-07 16:21:45 +0200

    Completed Exercise

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// DATI RICHIESTI
+const busRideKm = parseFloat(prompt("Quanti kilometri devi percorrere?"));
+const passengerAge = parseInt(prompt("Quanti anni hai?"));
+
+// ELABORAZIONE DATI
+const ticketPrice = busRideKm * 0.21;
+console.log(ticketPrice);
+const under18Age = 18;
+const over65Age = 65;
+const ticketDiscount20 = (ticketPrice * 20) / 100;
+console.log(ticketDiscount20);
+const ticketDiscount40 = (ticketPrice * 40) / 100;
+console.log(ticketDiscount40);
+
+let finalTicketPrice; 
+
+if (passengerAge < under18Age) {
+    finalTicketPrice = ticketPrice - ticketDiscount20;
+} else if (passengerAge > over65Age) {
+    finalTicketPrice = ticketPrice - ticketDiscount40;
+} else {
+    finalTicketPrice = ticketPrice;
+}
+
+//  OUTPUT
+document.getElementById("final-ticket-price").innerHTML = finalTicketPrice.toFixed(2);
